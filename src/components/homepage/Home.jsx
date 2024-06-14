cf6e95a96282d2e6f705683dd55cffe7fcf82943
Vasyl <161526913+Vasaaa24@users.noreply.github.com>
2024-06-14 21:53:53 +0200

    second step

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -6,17 +6,15 @@ const Home = () => {
       <section className="home" id="home">
         <div className="home-content">
           <h1>
-            Team <span>Webfinity</span>
+          Objednejte si svoji <span>stránku</span> na míru.
           </h1>
-          <h3 className="text-animation">
-            Objednejte si <span></span>
-          </h3>
+          
           <p>
             Nezávazně nás kontaktujte a sdělte nám svou představu o ideální
             webové stránce. Postaráme se o všechny technické a designové
             aspekty, aby vaše přání proměnila ve skutečnost. Vytvoříme web,
             který bude vizuálně atraktivní, funkční a uživatelsky přívětivý.
-            Spolehněte se na nás a společně dosáhneme vašeho cíle.
+            Spolehněte se na nás !
           </p>
           <div className="btn-group">
             <a href="#contact" className="btn">
